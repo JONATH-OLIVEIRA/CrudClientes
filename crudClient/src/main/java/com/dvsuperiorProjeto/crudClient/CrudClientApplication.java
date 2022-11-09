7e2054fa99186a419cc5b56260617a0d96a9f8dd
Jonath Oliveira <akiles22a@gmail.com>
2022-11-09 16:03:01 -0300

    Criacao de cliente e teste de listagem

--- crudClient/src/main/java/com/dvsuperiorProjeto/crudClient/CrudClientApplication.java
+++ crudClient/src/main/java/com/dvsuperiorProjeto/crudClient/CrudClientApplication.java
@@ -9,7 +9,7 @@ public class CrudClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudClientApplication.class, args);
 		
-		System.out.println("Test");
+		
 	}
 
 }
