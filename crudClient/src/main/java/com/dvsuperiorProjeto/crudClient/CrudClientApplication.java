5448f8cd15f272eaf4757cd196845028309ee6fe
Jonath Oliveira <akiles22a@gmail.com>
2022-11-09 14:08:52 -0300

    Link com Git

--- crudClient/src/main/java/com/dvsuperiorProjeto/crudClient/CrudClientApplication.java
+++ crudClient/src/main/java/com/dvsuperiorProjeto/crudClient/CrudClientApplication.java
@@ -8,6 +8,8 @@ public class CrudClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudClientApplication.class, args);
+		
+		System.out.println("Teste");
 	}
 
 }
