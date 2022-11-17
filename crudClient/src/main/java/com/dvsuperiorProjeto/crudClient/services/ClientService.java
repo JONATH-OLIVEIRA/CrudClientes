8dc8b58f040c951a77a9fa2f55bbd836a3aedd68
Jonath Oliveira <akiles22a@gmail.com>
2022-11-16 22:44:44 -0300

    tratamento de excecoes e erros e buscar por id implementados

--- crudClient/src/main/java/com/dvsuperiorProjeto/crudClient/services/ClientService.java
+++ crudClient/src/main/java/com/dvsuperiorProjeto/crudClient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.dvsuperiorProjeto.crudClient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.dvsuperiorProjeto.crudClient.dto.ClientDto;
 import com.dvsuperiorProjeto.crudClient.entities.Client;
 import com.dvsuperiorProjeto.crudClient.repositories.ClientRepository;
+import com.dvsuperiorProjeto.crudClient.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -25,6 +27,12 @@ public class ClientService {
 		
 		return list.stream().map(x -> new ClientDto(x)).collect(Collectors.toList());
 
+	}
+	@Transactional
+	public ClientDto findaById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(()-> new EntityNotFoundException("Nao Encontrado"));
+		return new ClientDto(entity);
 	}
 
 }
